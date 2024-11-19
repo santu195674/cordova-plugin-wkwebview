8448109e8145479cc0c5fc78d04c14bf3ef0d3e8
Santu Chakraborty <santuc@amdocs.com>
2024-11-19 19:56:45 +0530

    Change to hide header

--- src/ios/WKWebViewPlugin.m
+++ src/ios/WKWebViewPlugin.m
@@ -10,7 +10,6 @@
 - (void)open:(CDVInvokedUrlCommand*)command {
     NSString* urlString = [command.arguments objectAtIndex:0];
     NSNumber* showCloseButton = (command.arguments.count > 1) ? [command.arguments objectAtIndex:1] : @NO;
-    
     if (urlString == nil) {
         CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"URL cannot be null"];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
@@ -44,34 +43,38 @@
         CGFloat safeAreaTop = self.viewController.view.safeAreaInsets.top;
         topBarHeight += safeAreaTop;
     }
+    if (![showCloseButton boolValue]){
+        topBarHeight = topBarHeight - 50.0;
+    }
     
     webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, topBarHeight, self.viewController.view.bounds.size.width, self.viewController.view.bounds.size.height - topBarHeight) configuration:config];
     webView.UIDelegate = self;
     webView.navigationDelegate = self;
     [webView loadRequest:[NSURLRequest requestWithURL:url]];
     
-    // Create the top bar
-    UIView *topBar = [[UIView alloc] initWithFrame:CGRectMake(0, self.viewController.view.safeAreaInsets.top, self.viewController.view.bounds.size.width, 50)];
-    topBar.backgroundColor = [UIColor colorWithWhite:1.0 alpha:1.0]; // Very light gray color
-    topBar.autoresizingMask = UIViewAutoresizingFlexibleWidth; // Allows resizing on device rotation
-    
-    // Add a bottom border to the custom navigation bar
-    UIView *bottomBorder = [[UIView alloc] initWithFrame:CGRectMake(0, topBar.frame.size.height - 1, topBar.frame.size.width, 1)];
-    bottomBorder.backgroundColor = [UIColor lightGrayColor];
-    bottomBorder.autoresizingMask = UIViewAutoresizingFlexibleWidth; // Allows resizing on device rotation
-    
-    [topBar addSubview:bottomBorder];
     
     // Conditionally create the close button
     if ([showCloseButton boolValue]) {
+        // Create the top bar
+        UIView *topBar = [[UIView alloc] initWithFrame:CGRectMake(0, self.viewController.view.safeAreaInsets.top, self.viewController.view.bounds.size.width, 50)];
+        topBar.backgroundColor = [UIColor colorWithWhite:1.0 alpha:1.0]; // Very light gray color
+        topBar.autoresizingMask = UIViewAutoresizingFlexibleWidth; // Allows resizing on device rotation
+        
+        // Add a bottom border to the custom navigation bar
+        UIView *bottomBorder = [[UIView alloc] initWithFrame:CGRectMake(0, topBar.frame.size.height - 1, topBar.frame.size.width, 1)];
+        bottomBorder.backgroundColor = [UIColor lightGrayColor];
+        bottomBorder.autoresizingMask = UIViewAutoresizingFlexibleWidth; // Allows resizing on device rotation
+        
+        [topBar addSubview:bottomBorder];
+       
         UIButton *closeButton = [UIButton buttonWithType:UIButtonTypeSystem];
         closeButton.frame = CGRectMake(self.viewController.view.bounds.size.width - 60, 10, 50, 30);
         [closeButton setTitle:@"Close" forState:UIControlStateNormal];
         [closeButton addTarget:self action:@selector(closeWebView) forControlEvents:UIControlEventTouchUpInside];
         [topBar addSubview:closeButton];
+        [containerView addSubview:topBar];
     }
     
-    [containerView addSubview:topBar];
     [containerView addSubview:webView];
     
     
